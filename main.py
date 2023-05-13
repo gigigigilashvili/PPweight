10b503d9fa04f8959f57bb9b3e5c43b95765994c
gigigigilashvili <115583460+gigigigilashvili@users.noreply.github.com>
2023-05-13 13:10:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,30 @@ print(WeeklyAverage())
 Weight.close()
 Number_Of_Days.close()
 weekly_average.close()
+
+
+
+
+# Weight = open('weight', 'a')
+Number_Of_Days = open('number of days.txt', 'a')
+weekly_average = open('Weekly Average.txt', 'a')
+day = 0
+while True:
+    j = int(input('weight: '))
+    if j==0: break
+    day += 1
+    Number_Of_Days.writelines("Day "+str(day)+" "+str(j)+'\n')
+
+# weight = f'weight {j}'
+# W = str((days, weight))
+# Weight.writelines(W)
+# def WeeklyAverage():
+#     for i, k in Weight.readline():
+#         WeeksAverage = sum(k)
+#         return WeeksAverage
+#     weekly_average.writelines(str(WeeklyAverage()))
+#
+# print(WeeklyAverage())
+# Weight.close()
+# Number_Of_Days.close()
+# weekly_average.close()
